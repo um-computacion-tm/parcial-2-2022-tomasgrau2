51f7b11c1599e693fb61f3f59bcbe902b9d7e500
tomasgrau2 <t.grau@alumno.um.edu.ar>
2022-10-26 10:40:58 -0300

    R

--- pruebas.py
+++ pruebas.py
@@ -1,9 +1,9 @@
 
-# Con esta práctica se consigue, entre otras cosas, un código más robusto, más seguro, más mantenible y una mayor rapidez en el desarrollo.
+text= 'Con esta práctica se consigue, entre otras cosas, un código más robusto, más seguro, más mantenible y una mayor rapidez en el desarrollo.'
 # En este post voy a centrarme solamente en cómo TDD afecta al diseño de software, si queréis más información, hay una introducción bastante buena en la Wikipedia y Carlos Blé tiene disponible online un libro muy completo. Además, en esta infografía os contamos en qué consiste TDD, en qué principios se basa (SOLID) y cuáles son sus ventajas y desventajas. Y, si quieres profundizar aún más, te recomendamos que le eches un vistazo a TDD, una metodología para gobernarlos a todos y Molecule: desarrollo TDD en Ansible.  "
 
-# split = text.split(sep=' ')
-# # print(split)
+split = text.split(sep=' ')
+print(split)
 
 
 # split_2 = []
